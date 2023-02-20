864e12d30ed26a14afbb7f702176ca89931ce646
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-20 10:44:49 +0200

    added new execise to fCC_20.02

--- Freecodecamp.js/20.02.js
+++ Freecodecamp.js/20.02.js
@@ -4,3 +4,6 @@ const myStr1 = "This is the start. "+"This is the end.";
 
 let myStr2="This is the first sentence. ";
 myStr += "This is the second sentence."
+
+const myName = "Daria";
+const mySt3 = "My name is"+myName +"and I am well!";
