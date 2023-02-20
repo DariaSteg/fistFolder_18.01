89ae5e83fae983e0fb97f20de9bfbadea4f040e0
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-20 20:58:15 +0200

    added myData3

--- Freecodecamp.js/20.02.js
+++ Freecodecamp.js/20.02.js
@@ -48,3 +48,13 @@ const myArray3 = [50, 60, 70];
 const myData = myArray[0];
 
 myArray[0] = 45;
+
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData3 = myArray[2][1];
