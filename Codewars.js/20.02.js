ac8d6e9e63edc5c5b36a369cdcd92f64b2f0b9d4
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-20 20:27:50 +0200

    edded next execise

--- Codewars.js/20.02.js
+++ Codewars.js/20.02.js
@@ -1,3 +1,9 @@
 var cubeChecker = function(volume, side){
     if(((volume/side)/side) == side && ((volume/side)/side)>0){return true;}return false;
   }
+
+  function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return results;
+  }
