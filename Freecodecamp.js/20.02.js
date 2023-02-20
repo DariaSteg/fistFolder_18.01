a13264a253eb07cf022d00c8061321dfc5eca9c9
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-20 10:38:25 +0200

    added nex execise to Freecodecamp_20.02

--- Freecodecamp.js/20.02.js
+++ Freecodecamp.js/20.02.js
@@ -1,3 +1,6 @@
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
 
 const myStr1 = "This is the start. "+"This is the end.";
+
+let myStr2="This is the first sentence. ";
+myStr += "This is the second sentence."
