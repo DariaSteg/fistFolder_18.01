b5c3bee645d258c6af7878f5b9b4773039be5553
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-21 10:59:58 +0200

    added .unshift

--- Freecodecamp.js/21.02.js
+++ Freecodecamp.js/21.02.js
@@ -1,2 +1,8 @@
 const myArray = [["John", 23], ["dog", 3]];
 const removedFromMyArray = myArray.shift()
+
+const myArray2 = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+
+myArray.unshift(["Paul", 35]);
