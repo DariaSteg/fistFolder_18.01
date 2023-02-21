d919c4bf6eda8fded4074192952940b5d3161d5e
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-21 11:34:02 +0200

    create first function

--- Freecodecamp.js/21.02.js
+++ Freecodecamp.js/21.02.js
@@ -9,3 +9,9 @@ myArray.unshift(["Paul", 35]);
 
 const myList =[["T-shirt", 15], ["Dart", 12], ["long", 8],
 ["long", 5],["long", 0]];
+
+function reusableFunction() {
+    console.log("Hi World");
+   }
+   
+   reusableFunction();
