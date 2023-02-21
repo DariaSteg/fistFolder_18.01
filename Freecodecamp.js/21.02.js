7f6c7e60a13f419dfaf29bd04535a89dfb139a6c
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-21 12:23:45 +0200

    added fun1

--- Freecodecamp.js/21.02.js
+++ Freecodecamp.js/21.02.js
@@ -24,3 +24,22 @@ function timesFive(number){
    }
    timesFive(5);
 
+   const myGlobal=10;
+
+function fun1() {
+ oopsGlobal=5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
