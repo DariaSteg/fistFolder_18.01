a4987f1e1e0d93b6230f86a7843d4d5937212f2a
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-22 19:18:36 +0200

    added testStrictNotEqual

--- Freecodecamp.js/22.02.js
+++ Freecodecamp.js/22.02.js
@@ -108,3 +108,12 @@ function welcomeToBooleans() {
   }
   
   testNotEqual(10);
+
+  function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
