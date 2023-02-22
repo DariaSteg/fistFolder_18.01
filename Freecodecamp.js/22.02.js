c3c8231f00c7a7a1a69214e9b35854d60e39ecb6
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-22 19:20:55 +0200

    add testGreaterThan

--- Freecodecamp.js/22.02.js
+++ Freecodecamp.js/22.02.js
@@ -117,3 +117,17 @@ function welcomeToBooleans() {
   }
   
   testStrictNotEqual(10);
+
+  function testGreaterThan(val) {
+    if (val> 100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
