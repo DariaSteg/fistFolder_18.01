d92c5f88dadcfd455bf52218c9a9c6680f2fda05
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-22 19:42:35 +0200

    testLessOrEqual

--- Freecodecamp.js/22.02.js
+++ Freecodecamp.js/22.02.js
@@ -173,3 +173,16 @@ function welcomeToBooleans() {
   }
   
   testLessOrEqual(10);
+
+  function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val <= 50 && val>=25) {
+        return "Yes";
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
