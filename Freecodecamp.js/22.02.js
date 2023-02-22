4788f91b3d277a72d8a5849c45f87bdce2f2c319
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-22 19:54:45 +0200

    added testElse

--- Freecodecamp.js/22.02.js
+++ Freecodecamp.js/22.02.js
@@ -199,3 +199,20 @@ function welcomeToBooleans() {
   }
   
   testLogicalOr(15);
+
+  function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+      else {
+      result = "5 or Smaller";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  testElse(4);
