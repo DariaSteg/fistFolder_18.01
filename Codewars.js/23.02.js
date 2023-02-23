6c2ada4aef47266c72257c46e26bbb5f7b60e594
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-23 10:37:17 +0200

    added describeAge

--- Codewars.js/23.02.js
+++ Codewars.js/23.02.js
@@ -1,3 +1,9 @@
 function litres(time) {
     return Math.trunc(time * 0.5);
   }
+
+  function describeAge(age) {
+    return age <= 12?"You're a(n) kid":age >= 13 && age <= 17?"You're a(n) teenager":age >= 18 && age <= 64?
+      "You're a(n) adult":"You're a(n) elderly";
+   
+   }
