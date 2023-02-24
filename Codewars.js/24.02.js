10f428522104767744726c04a00886297bb8fd9a
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-02-24 09:59:51 +0200

    added zeroFuel

--- Codewars.js/24.02.js
+++ Codewars.js/24.02.js
@@ -3,3 +3,7 @@ function chromosomeCheck(sperm) {
     return "Congratulations! You're going to have a son."}
     return "Congratulations! You're going to have a daughter.";
   }
+
+  const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return fuelLeft * mpg == distanceToPump?true:false;
+   };
