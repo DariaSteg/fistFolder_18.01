c1b90889bd8b1f95baeb845de53f3f51f66549bc
Daria Stegura <97463847+DariaSteg@users.noreply.github.com>
2023-03-02 16:32:45 +0200

    Codewars added

---
+++ Codewars.js/02.03.js
@@ -0,0 +1,61 @@
+function greet(language) {
+    let answer = "";
+     switch(language){
+       case "english":
+       answer='Welcome';
+       break;
+       case "czech":
+       answer= 'Vitejte';
+       break;
+       case "danish":
+       answer ='Velkomst';
+       break;
+       case "dutch":
+       answer ='Welkom';
+       break;  
+       case "estonian":
+       answer ='Tere tulemast';
+       break;
+       case "finnish":
+       answer ='Tervetuloa';
+       break;
+       case "flemish":
+       answer ='Welgekomen';
+       break;
+       case "french":
+       answer ='Bienvenue';
+       break;
+       case "german":
+       answer ='Willkommen';
+       break;
+       case "irish":
+       answer ='Failte';
+       break;
+       case "italian":
+       answer ='Benvenuto';
+       break;
+       case "latvian":
+       answer ='Gaidits';
+       break; 
+       case "lithuanian":
+       answer ='Laukiamas';
+       break;
+       case "polish":
+       answer ='Witamy';
+       break;
+       case "spanish":
+       answer ='Bienvenido';
+        break;
+        case "swedish":
+        answer ='Valkommen';
+         break;
+        case "welsh":
+        answer ='Croeso';
+         break;
+         default:
+         answer='Welcome';
+         break;
+     
+     
+     }
+    }   
